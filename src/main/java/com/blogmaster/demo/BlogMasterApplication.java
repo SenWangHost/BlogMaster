18c22b59b1e9f623abaca1c036991c3cfd3d8b8a
SenWangHost <senwang1992010@gmail.com>
2018-06-16 19:21:33 -0400

    Initialize the blog master project with spring boot initializer

---
+++ src/main/java/com/blogmaster/demo/BlogMasterApplication.java
@@ -0,0 +1,12 @@
+package com.blogmaster.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogMasterApplication.class, args);
+	}
+}
