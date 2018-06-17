02b850cd13fc63def1798ee4c149de2e4dd50b47
SenWangHost <senwang1992010@gmail.com>
2018-06-16 20:32:29 -0400

    set up and link the login and register page

--- src/main/java/com/blogmaster/demo/Controller/BlogMasterController.java
+++ src/main/java/com/blogmaster/demo/Controller/BlogMasterController.java
@@ -8,10 +8,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class BlogMasterController {
     /**
-     * the mapping to the homepage
+     * the mapping to the homepage page
      */
     @RequestMapping(value = {"/", "/homepage"}, method = RequestMethod.GET)
     public String homePage() {
         return "homepage";
+    }
+    /**
+     * the mapping to the login page
+     */
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String loginPage() {
+        return "login";
+    }
+    /**
+     * the mapping to the register page
+     */
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    public String registerPage() {
+        return "register";
     }
 }
